1a633fb50ea31d3847a7950da76a41a22ddc6d41
Bunyawat Srisompong <bunyawat@blockfint.com>
2025-04-26 22:11:13 +0700

    refactor: remove Google OAuth provider from AppProvider and update dependencies
    Eliminate the GoogleOAuthProvider from the AppProvider component and remove its dependency from package.json.

--- providers/AppProvider.tsx
+++ providers/AppProvider.tsx
@@ -1,14 +1,11 @@
 'use client';
 
 import React from 'react';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { MantineProvider } from '@mantine/core';
 import { ModalsProvider } from '@mantine/modals';
 import { Notifications } from '@mantine/notifications';
-// config
-import { getGoogleClientSecret } from '@/config/constants';
 // lib
 import { queryClient } from '@/libs/react-query';
 // theme
@@ -20,17 +17,15 @@ type AppProviderProps = {
 
 const AppProvider = ({ children }: AppProviderProps) => {
   return (
-    <GoogleOAuthProvider clientId={getGoogleClientSecret()}>
-      <MantineProvider theme={theme} cssVariablesResolver={cssVariableResolver}>
-        <ModalsProvider>
-          <QueryClientProvider client={queryClient}>
-            <Notifications />
-            {children}
-            <ReactQueryDevtools initialIsOpen={false} />
-          </QueryClientProvider>
-        </ModalsProvider>
-      </MantineProvider>
-    </GoogleOAuthProvider>
+    <MantineProvider theme={theme} cssVariablesResolver={cssVariableResolver}>
+      <ModalsProvider>
+        <QueryClientProvider client={queryClient}>
+          <Notifications />
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </ModalsProvider>
+    </MantineProvider>
   );
 };
 
