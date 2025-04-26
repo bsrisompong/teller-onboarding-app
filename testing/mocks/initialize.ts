06e1f672193e7296d21c0cef7eb7e265c41b84f7
Bunyawat Srisompong <bunyawat@blockfint.com>
2025-04-26 21:22:03 +0700

    refactor: clean up mock initialization by removing unnecessary console logs
    Remove console log statements related to environment variables in the mock initialization file to streamline the code and improve readability.

--- testing/mocks/initialize.ts
+++ testing/mocks/initialize.ts
@@ -1,11 +1,6 @@
 // import { seedDb } from './seed-db';
 
-import { IS_BROWSER, IS_SERVER } from '@/config/constants';
-
 const initializeMocks = async () => {
-  console.log('process.env.NEXT_RUNTIME', process.env.NEXT_RUNTIME);
-  console.log('IS_BROWSER', IS_BROWSER);
-  console.log('IS_SERVER', IS_SERVER);
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     const { server } = await import('./server');
     server.listen();
